9c2a74bbaa55a31147a402b63c7d7ab66f265a27
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-10-16 19:05:26 -0500

    BNR int pt 2

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -16,21 +16,37 @@ class ViewController: UIViewController {
         
         
         /**** DO STUFF HERE ****/
-       print("The maximum Int value is \(Int.max).")
-       print("The minimum Int value is \(Int.min).")
-        print("The maximum UInt value is \(UInt.max).")
-        print("The minimum UInt value is \(UInt.min).")
-        print("The maximum value for a 32-but unsigned integer is \(UInt32.max).")
-        print("The minimum value for a 32-but unsigned integer is \(UInt32.min).")
+        var someCharacter:Character = "c"
+        
+        switch someCharacter {
+        case "a":
+                print("is an A")
 
-        let numberOfPages: Int = 10 // Declares the type explicity
-        let numberOfChapters = 3 // Also of type Int, but inferred by the compiler
+        case "b":
+                print("is a B")
+
+        default:
+            print("some fallback")
+        }
         
-        let numberOfPeople: UInt = 40
-        let volumeAdjustment: Int32 = -1000
+        for index in 1...5 {
+            print("hello")
+        }
+        for index in 1...5 {
+            print(index)
+        }
         
-        print(11 / 3)
+        for index in 1...5 {
+            var sum = 0
+            sum += index
+            print(sum)
+        }
         
+        var sum = 0
+        for index in 1...5 {
+            sum += index
+        }
+        print(sum)
         
         /**** END: DO STUFF HERE ****/
         
