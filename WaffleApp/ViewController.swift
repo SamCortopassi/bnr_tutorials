9601334fc5de0619ad87134cb5d225a046a3492a
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-11-20 15:52:57 -0600

    array

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -14,34 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+      
+       var d = ["dog", "cat", "bird"]
+        var e = [String]()
         
+        d += ["mouse", "owl"]
+        
+        
+        for item in d {
+        print("my " + item)
+        }
+
         /**** DO STUFF HERE ****/
         
-        let point = (x: 1, y: 4)
-        
-        switch point {
-        case let q1 where (point.x > 0) && (point.y > 0):
-            print("\(q1) is in quadrant 1")
-        
-        case let q2 where (point.x < 0) && (point.y > 0):
-            print("\(q2) is in quadrant 2")
-            
-        case let q3 where (point.x < 0) && (point.y < 0):
-            print ("\(q3) is in quadrant 3")
-            
-        case let q4 where (point.x > 0) && (point.y < 0) :
-            print ("\(q4) is in quadrant 3")
-            
-        case (_, 0) :
-            print("\(point) sits on the x-asis")
-            
-        case (0, _) :
-            print("\(point) sits on the y-axis")
-            
-        default:
-            print("case not covered")
-            
-        }
+      
         
         /**** END: DO STUFF HERE ****/
         
