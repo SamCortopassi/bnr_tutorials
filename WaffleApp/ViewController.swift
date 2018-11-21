0d55c6480723e5fb31cffe99ddfbde6ffdcbf76c
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-11-20 20:54:36 -0600

    array mods

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -14,23 +14,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-      
-       var d = ["dog", "cat", "bird"]
-        var e = [String]()
+       
         
-        d += ["mouse", "owl"]
-        d.remove(at: 3)
-        
-        for item in d {
-        print("my " + item)
-        }
-
-        d.count
-        
-        print (d.count)
         /**** DO STUFF HERE ****/
         
-      
+    var bucketList = ["Climb Mt Everest"]
+        bucketList.append("Fly hot air balloon to Fiji.")
+        bucketList.append("watch LOTR")
+        bucketList.append("walkabout")
+        bucketList.append("Great blue hole")
+        bucketList.append("Triple Rainbow")
+        bucketList[3] += " in Australia"
+        bucketList[0] = "Climb Kilimanjaro"
+        
+        
+        var bucketList2 = ["Climb hill"]
+        var newItems = ["hot air balloon", "Watch Harry Potter", "vacation Italy", "surf", "cattle drive"]
+        
+        for item in newItems {bucketList2.append(item)
+        }
+        bucketList2[4] += " in HI"
+        bucketList2[5] = "push steer"
+        
+        
+        print(bucketList2)
+        
+        
         
         /**** END: DO STUFF HERE ****/
         
