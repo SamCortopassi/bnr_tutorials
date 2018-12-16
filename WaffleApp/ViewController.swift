b178f903d345bdb33140b4e7d748b334c1790f64
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-15 21:44:47 -0600

    binding

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -21,9 +21,10 @@ class ViewController: UIViewController {
         var errorCodeString: String?
        errorCodeString = "404"
        // print(errorCodeString)
-        if errorCodeString != nil {
-            let theError = errorCodeString!
-            print(theError)
+        //if errorCodeString != nil {
+         //   let theError = errorCodeString!
+        if let theError = errorCodeString {
+        print(theError)
         }
        
         
