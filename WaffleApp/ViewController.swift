d9fd0fd6943f196b8a5f45e385fae930d97b838d
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-03 18:01:37 -0600

    mod dict

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -17,55 +17,14 @@ class ViewController: UIViewController {
        
         
         /**** DO STUFF HERE ****/
-//
-       var bucketList = ["Climb Mt. Everest"]
-//        bucketList.append("Fly hot air balloon to Fiji")
-//        bucketList.append("watch LOTR trilogy in one day")
-//        bucketList.append("go on a walkabout")
-//        bucketList.append("Scuba dice in the Great Blue Hole")
-//        bucketList.append("Find a triple rainbow")
-        
-        
-var newItems = [
-    "Climb Mt. Everest",
-    "Fly hot air balloon to Fiji",
-    "Watch LOTR trilogy in one day",
-    "Go on a walkabout",
-    "Scuba dice in the Great Blue Hole",
-    "Find a triple rainbow"
-    ]
-        
-//        for item in newItems {
-//            bucketList.append(item)
-//        }
-        bucketList += newItems
 
-        bucketList.remove(at: 2)
-        bucketList[0] = "Climb Mt. Kilimanjaro"
+        var dict1: Dictionary<String, Double> = [:]
+        var dict2 = Dictionary<String, Double>()
+        var dict3: [String:Double] = [:]
+        var dict4 = [String:Double]()
         
-        print(bucketList.count)
-        bucketList[2] += " in Australia"
-        print(bucketList[4])
-        print(bucketList[0...2])
-        bucketList.insert("Toboggan across AK", at: 2)
-        print (bucketList)
-        
-        var myronsList = [
-            "Climb mt> Kilimanjaro",
-            "Fly hot air balloon to Fiji",
-            "Go on a walkabout in Australia",
-            "Scuba dive in the Great Blue Hole",
-            "Find a triple rainbow"
-            ]
-        
-        let equal = (bucketList == myronsList)
-        
-        let lunches = [
-            "cheeseburger",
-            "veggie pizza",
-            "ckn cesar salad",
-            "Falafel wrap"
-        ]
+        var movieRatings = ["Donnie Darko": 4, "Chungking Ezpress": 5, "Dark City": 4]
+        print("I have rated \(movieRatings.count) movies")
         
         /**** END: DO STUFF HERE ****/
         
