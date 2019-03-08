04762b657b8c75415617b42030af0edbc042694e
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-03-08 14:29:26 -0600

    fizzbuzz

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -6,53 +6,37 @@
 //  Copyright © 2018 Samantha Cortopassi. All rights reserved.
 //
 
+
 import UIKit
 
-//class ViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        
-//        var str = "hello, playground"
-//        var playgroundGreeting = str
-//        playgroundGreeting += "! How are you today"
-////        print(str)
-//    
-//        class GreekGod {
-//            var name: String
-//            init(name: String) {
-//                self.name = name
-//            }
-//        }
-//        let hecate = GreekGod(name: "Hecate")
-//        let anotherHecate = hecate
-//        anotherHecate.name = "AnotherHecate"
-////        anotherHecate.name
-////        hecate.name
-//        struct Pantheon {
-//            var chiefGod: GreekGod
-//        }
-//        let pantheon = Pantheon(chiefGod: hecate)
-//        let zeus = GreekGod(name: "Zeus")
-////        pantheon.chiefGod = zeus
-//        zeus.name = "Zeus Jr."
-//        
-//        print("\(zeus)")
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
+        /**** DO STUFF HERE ****/
+//        fizz %3 buzz %5
+        //        for i in 1...20 {
+//
+        var myFirstInt: Int = 0
+        
+        for i in 1...50 { if i % 3 == 0 && i % 5 == 0 { print("\(i) FIZZBUZZ")
+            } else { if i % 3 == 0 { print("\(i) fizz")
+            } else { if i % 5 == 0 { print("\(i) buzz") }
+            }
+        }
+        }
         
         /**** END: DO STUFF HERE ****/
         
-
-//
-//func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//         Dispose of any resources that can be recreated.
-////    }
-//}
-//}
-//
-////        let coffee = ["coffee", "expresso", "cappuccino", "latte", "mocha", "apple"]
-//        let longest = coffee.sorted(by: )
-
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
 
