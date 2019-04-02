1f1b4c38dfb0cab510bc914a6a103ae91e6e9ae9
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-04-02 16:59:34 -0500

    url session

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -19,27 +19,32 @@ class ViewController: UIViewController {
 //        fizz %3 buzz %5
         //        for i in 1...20 {
 //
-      
-        for i in 1...20 {
-        if i % 3 == 0 && i % 5 == 0 {
-            print("\(i) FizzBuzz")
-        } else if i % 3 == 0 {
-            print("\(i) F")
-        } else if i % 5 == 0 {
-            print("\(i) B")
+        let configuration = URLSessionConfiguration.default
+        configuration.waitsForConnectivity = true
+        let session = URLSession(configuration: configuration)
+        
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")
+        
+        let task = session.dataTask(with: url!) {
+            
+            (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                return
+            }
+            guard let data = data else {
+                print(error.debugDescription)
+                return
         }
-    }
-    
+            if let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String? {
+                print(result)
+            }
     
         /**** END: DO STUFF HERE ****/
         
     }
-    
-    override func didReceiveMemoryWarning() {
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
-
+}
