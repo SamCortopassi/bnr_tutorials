7fe90448d580f9dfbcf2024f95a953f41d6700e6
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-04-03 23:12:55 -0500

    fb 2

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -19,26 +19,19 @@ class ViewController: UIViewController {
 //        fizz %3 buzz %5
         //        for i in 1...20 {
 //
-        let configuration = URLSessionConfiguration.default
-        configuration.waitsForConnectivity = true
-        let session = URLSession(configuration: configuration)
-        
-        let url = URL(string: "https://jsonplaceholder.typicode.com/users")
-        
-        let task = session.dataTask(with: url!) {
-            
-            (data, response, error) in
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                return
+        for i in 1...100
+        {
+            if i % 3 == 0 && i % 5 == 0 {
+                print("FizzBuzz")
+            } else if i % 3 == 0 {
+                print("Fizz")
+            } else if i % 5 == 0 {
+                print("Buzz")
+            } else {
+                print(i)
             }
-            guard let data = data else {
-                print(error.debugDescription)
-                return
         }
-            if let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String? {
-                print(result)
-            }
-    
+
         /**** END: DO STUFF HERE ****/
         
     }
