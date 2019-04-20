6e65f9bcc7929772bd2e507ac57699175f54c65f
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-04-19 22:40:40 -0500

    FB

--- WaffleApp/ViewController.swift
+++ WaffleApp/ViewController.swift
@@ -19,33 +19,26 @@ class ViewController: UIViewController {
 //        fizz %3 buzz %5
         //        for i in 1...20 {
 //
-        let json = "{ 'hello' : 'world'}"
-        let url = URL(string: "http://httpbin.org/post")!
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "Post"
-        request.httpBody = json.data(using: .utf8)
-        
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request)
-        
-            (data, response, error) in
-        if let data = data {
-            if let postResponse = String(data: data, encoding: .utfi) {
-                print(postResponse)
-            }
+        for i in 1...100
+        {
+            if i % 3 == 0 && i % 5 == 0 {
+                print("FizzBuzz")
+            } else if i % 3 == 0 {
+                print("Fizz")
+            } else if i % 5 == 0 {
+                print("Buzz")
+            } else {
+                print(i)
             }
         }
-    task.resume()
-    
-}
+
 
         /**** END: DO STUFF HERE ****/
         
-    }
-    override func didReceiveMemoryWarning() {
+
+func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
+
 
